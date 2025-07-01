a39a2973c151b50c658b588747f3ed0fd6fb681c
lisein <liseinjon@hotmail.com>
2025-07-01 09:16:50 +0200

    irm2walsi

--- README.md
+++ README.md
@@ -8,6 +8,34 @@ Nc file are very convenient, for example for processing with cdo software. This 
 
 > ./irm2nc --in /home/jo/Documents/Scolyte/Data/climat_Sco/IRM/climateGrid/climategrid_202408.csv --inputGrid /home/jo/app/irmGriddedObs2nc/in/grilleIRMGDL.nc --inputGridDef /home/jo/app/irmGriddedObs2nc/in/gridIRMGDL.txt --out=/home/jo/Documents/Scolyte/Data/climat_Sco/IRM/climateGrid/test.nc
 
+script:
+
+#!/bin/bash
+
+for i in {1..9}
+do
+    ./irm2nc --in /home/jo/Documents/Scolyte/Data/climat_Sco/IRM/climateGrid/climategrid_20240$i.csv --inputGrid /home/jo/app/irmGriddedObs2nc/in/grilleIRMGDL.nc --inputGridDef /home/jo/app/irmGriddedObs2nc/in/gridIRMGDL.txt --out=/home/jo/Documents/Scolyte/Data/climat_Sco/IRM/climateGrid/20240$i.nc
+done
+
+for i in {10..12}
+do
+    ./irm2nc --in /home/jo/Documents/Scolyte/Data/climat_Sco/IRM/climateGrid/climategrid_2024$i.csv --inputGrid /home/jo/app/irmGriddedObs2nc/in/grilleIRMGDL.nc --inputGridDef /home/jo/app/irmGriddedObs2nc/in/gridIRMGDL.txt --out=/home/jo/Documents/Scolyte/Data/climat_Sco/IRM/climateGrid/2024$i.nc
+done
+
+cdo copy 2024*.nc IRM-2024.nc
+
+
+## Other specifi usage: export txt file of monthly climate for one XY position for usage in Walsi module (CAPSIS)
+this tool (outil number 2 called by --outil 2) use as input a directory containing year-based file of monthly gridded-observation. Daily to montly observations may be computed with cdo with the following call (one call for each year): 
+
+cdo -b F64 merge -monsum -selname,RF,ETP /home/jo/Documents/Scolyte/Data/climat_Sco/IRM/nc/IRM-griddedObservation/IRM-1960.nc -monmean -selname,TG /home/jo/Documents/Scolyte/Data/climat_Sco/IRM/nc/IRM-griddedObservation/IRM-1960.nc /home/jo/Documents/Scolyte/Data/climat_Sco/IRM/nc/IRM-griddedObservation/MAR-monthly/IRM-monthly-1960.nc
+
+then time serie for one pixel is extracted and written in a txt file with this command. Toto and tata argument value aren't used, there are only there because the arguments "inputGrid" and "inputGridDef" are mandatory (for tool number 1)
+
+./irm2nc --in /home/jo/Documents/Scolyte/Data/climat_Sco/IRM/nc/IRM-griddedObservation/MAR-monthly --inputGrid toto --inputGridDef tata --out=/home/jo/Documents/suiviTempMAB/data/capsis/irm2walsi.txt --outil 2 --X 60000 --Y 6300000
+
+Be catious, the position of the forest site you provide with X and Y is in the coordinate system of the IRM grid (+proj=lcc +lat_2=50.569898649999999 +lat_1=50.569898649999999 +lon_0=4.553615160000000 +units=m +no_defs +a=6371229.0 +es=0.0) that can be added in QGis as a "custom CRS"
+
 ## Linux Installation
 
 >git clone https://github.com/JoLeBelge/rmiGriddedObs2nc
--- main.cpp
+++ main.cpp
@@ -12,8 +12,16 @@ std::string inputGrid;
 std::string inputGridDef;
 std::string pathOut;
 
+std::string irmFile(std::string dir,int y );
+
+// origin and resol of IRM grid
+int oX=-145000;
+int oY=6.44051e+06;
+double res=5000;// 5km
+
 int main(int argc, char *argv[])
 {
+    int mode=1;
     po::options_description desc("Allowed options");
     desc.add_options()
             ("help", "produce help message")
@@ -21,6 +29,9 @@ int main(int argc, char *argv[])
             ("inputGrid", po::value< std::string>()->required(), "directory of netcdf template for IRM containing the grid with associated pixel_id")
             ("inputGridDef", po::value< std::string>()->required(), "txt with definition of grid")
             ("out", po::value< std::string>()->required(), "file out")
+            ("outil", po::value<int>(), "outil ; def outil number 1")
+            ("X", po::value< double>()->required(), "position X (custom irm coordinate system)")
+            ("Y", po::value< double>()->required(), "position Y (custom irm coordinate system)")
             ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -33,10 +44,15 @@ int main(int argc, char *argv[])
         std::cout << " je vais utiliser le fichier de donnée IRM " << input << std::endl;
     }
     if (vm.count("inputGrid")) {inputGrid=vm["inputGrid"].as<std::string>();
-        std::cout << " je vais utiliser le nc template" << inputGrid << std::endl;}
+        std::cout << " je vais utiliser le nc template " << inputGrid << std::endl;}
     if (vm.count("inputGridDef")) {inputGridDef=vm["inputGridDef"].as<std::string>();}
     if (vm.count("out")) {pathOut=vm["out"].as<std::string>();
         std::cout << " j'écrirai les résutats dans le fichier " << pathOut << std::endl;}
+
+     if (vm.count("outil")) {mode=vm["outil"].as<int>();}
+
+     if (mode==1){
+
     std::cout << " create netcdf for IRM gridded observation" << std::endl;
     if (boost::filesystem::exists(input)){
         irmData d(input);
@@ -44,7 +60,75 @@ int main(int argc, char *argv[])
     } else {
         std::cout << " je ne peux rien faire car " << input << " n'existe pas..." <<std::endl;
     }
+
+     } else {
+       std::cout << " création d'un ficher de climat pour modèle Walsi à partir d'une série temporelle de fichier netcdf (situé dans 'in') mensuels et d'une position en région Wallonne" << std::endl;
+       double X=0;
+       double Y=0;
+       if (vm.count("X")) {X=vm["X"].as<double>();}
+       if (vm.count("Y")) {Y=vm["Y"].as<double>();}
+
+       // calcul de la position du pixel
+       int U(33),V(33);
+
+       // pas round, floor!
+       U=floor((X-oX)/res);
+       V=floor((-Y+oY)/res);
+       std::cout << "position of your forest site (X " << X << ", Y "<< Y << ") in pixel is " << U << ", " << V << std::endl;
+       // vérification  (indexbox commence à 1, pas comme c++ qui commence à 0):
+       // cdo -selindexbox,U+1,U+1,V+1,V+1 -selvar,TG IRM-monthly-1960.nc tmp.nc
+       // ncdump tmp.nc
+
+        std::ofstream ofs (pathOut, std::ofstream::out);
+        ofs << "#Year\tMonth\tMean_T\tSum_P\tSum_pet\n";
+       for (int y(1950);y<2025;y++){
+           std::cout << "année " << std::to_string(y) << std::endl;
+           std::string ncFile=irmFile(input,y);
+           const char * p=ncFile.c_str();
+           NcFile in(p,NcFile::FileMode::ReadOnly,NULL,0,NcFile::FileFormat::Netcdf4);
+           if (!in.is_valid())
+           {
+               std::cout << "Couldn't open file : " << ncFile << "!\n";
+           } else{
+
+               //std::cout << "input File has " << in.num_dims() << " dimensions and " <<in.num_vars() << " vars and "  << in.num_atts() << " atts" << std::endl ;
+               NcVar *varT, *varRF, *varETP;
+               //timeVar=in.get_var("TIME");
+               //NcDim * time=in.get_dim("TIME");
+               NcVar * lon=in.get_var("X");
+               int NLON = lon->num_vals();
+               NcVar * lat=in.get_var("Y");
+               int NLAT = lat->num_vals();
+               float var_in[NLAT][NLON];
+
+               //std::cout << "lon is " << NLON << ", lat "<< NLAT << std::endl;
+
+               //std::cout << "get var TG, RF and ETP" << std::endl;
+               varT=in.get_var("TG");
+               varRF=in.get_var("RF");
+               varETP=in.get_var("ETP");
+
+               for (int i(0); i <12;i++){
+                   int rec= i;
+                   varT->set_cur(rec, 0, 0);
+                   varT->get(&var_in[0][0], 1, NLAT, NLON);
+                    float T =  var_in[V][U];
+                   varRF->set_cur(rec, 0, 0);
+                   varRF->get(&var_in[0][0], 1, NLAT, NLON);
+                   float RF =  var_in[V][U];
+                   varETP->set_cur(rec, 0, 0);
+                   varETP->get(&var_in[0][0], 1, NLAT, NLON);
+                   float ETP =  var_in[V][U];
+                   ofs <<y<<"\t"<< i+1 << "\t" << T << "\t" << RF << "\t" << ETP << "\n";
+
+               }// end month
+           }// end test file exist
+
+     }// end year
+     }// end mode ==2
     std::cout << "done" << std::endl;
     return 0;
 }
+
+std::string irmFile(std::string dir,int y ){return dir+"/IRM-monthly-"+std::to_string(y)+".nc";}
 
